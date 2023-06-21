724335703462d150f0f1b958f99973d1c3ae4d43
isakleong <37265470+isakleong@users.noreply.github.com>
2023-06-21 17:02:57 +0700

    Update CatalogueController.php
    update dialog message

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -58,7 +58,7 @@ class CatalogueController extends Controller
 
         Catalogue::create($input);
 
-        return redirect('/admin/product/catalogue')->withSuccess('Data Added Successfully!');
+        return redirect('/admin/product/catalogue')->withSuccess('Catalogue added successfully!');
     }
 
     public function show(Catalogue $catalogue)
@@ -106,7 +106,7 @@ class CatalogueController extends Controller
             File::delete($imageDelete);
         }
 
-        return redirect('/admin/product/catalogue')->withSuccess('Data Updated Successfully!');
+        return redirect('/admin/product/catalogue')->withSuccess('Catalogue updated successfully!');
     }
 
     public function destroy(Catalogue $catalogue)
@@ -117,6 +117,6 @@ class CatalogueController extends Controller
         
         File::delete($imageDelete);
 
-        return redirect('/admin/product/catalogue')->withSuccess('Data Deleted Successfully!');
+        return redirect('/admin/product/catalogue')->withSuccess('Catalogue deleted Successfully!');
     }
 }
